20ba568189f6a28b07854ba2f48fb72d73613b3e
kocierik <kocierik@gmail.com>
2022-04-23 12:20:49 +0200

    feat: add typescript to react

--- front-office/src/App.tsx
+++ front-office/src/App.tsx
@@ -1,13 +1,12 @@
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,10 +16,9 @@ function App() {
         >
           Learn React
         </a>
-        <div className="erik">ciao</div>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
