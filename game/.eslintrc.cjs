4802aff99a466ab5cdd9097ce5ba598727e79124
kocierik <kocierik@gmail.com>
2022-04-23 18:12:46 +0200

    feat: add prittier & eslint

--- game/.eslintrc.cjs
+++ game/.eslintrc.cjs
@@ -1,15 +1,23 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:vue/vue3-essential',
     '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier',
+    'plugin:prettier/recommended',
   ],
   env: {
     'vue/setup-compiler-macros': true,
   },
-}
+  rules: {
+    // enable additional rules
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-empty': 'warn',
+    'no-cond-assign': ['error', 'always'],
+  },
+};
