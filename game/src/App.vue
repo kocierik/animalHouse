43e87297cca7823eff9172ac6afee533805c51ae
kocierik <kocierik@gmail.com>
2022-04-27 17:48:46 +0200

    chore: add game page

--- game/src/App.vue
+++ game/src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-   <h1 class="text-3xl font-bold underline">
-    Hello world! != =>
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello</h1>
 </template>
 
-<style>
-</style>
+<style></style>
---
+++ game/src/components/memoryGame/MemoryGame.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import './memory.css';
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <section class="main">Memory Game</section>
+</template>
