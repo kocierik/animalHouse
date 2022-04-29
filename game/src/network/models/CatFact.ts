c2422500f9ddc5d28f35052ce5a119729a3f580c
Mattia <mgiro2001@gmail.com>
2022-04-29 12:40:38 +0200

    added curiosity facts for cats

---
+++ game/src/network/models/CatFact.ts
@@ -0,0 +1,3 @@
+export default interface CatFact {
+  data : string[]
+}
--- game/src/router/index.ts
+++ game/src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
+/*
+import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -21,3 +21,4 @@ const router = createRouter({
 });
 
 export default router;
+*/
