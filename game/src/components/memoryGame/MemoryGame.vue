943fb180e64504cbb282ec189dab4e5737817394
kocierik <kocierik@gmail.com>
2022-04-30 19:34:17 +0200

    feat: add logical color

--- game/src/components/memoryGame/MemoryGame.vue
+++ game/src/components/memoryGame/MemoryGame.vue
@@ -5,23 +5,40 @@ import { ref } from 'vue';
 import type { Card } from './utility/cards';
 import cards from './utility/cards';
 
-const defaultCard: Card = { firstName: ' ', id: 0, view: 'visible', bg: '', selected: false };
+const defaultCard: Card = { firstName: ' ', id: 0, view: 'visible', bg: '', selected: false, opacity: 1 };
 
 let selectOne: Card = defaultCard;
 let selectTwo: Card = defaultCard;
 let result: Card[];
 const checkCard = (card: Card): void => {
   console.log(card.id);
-  // card.view == 'visible' ? (card.view = 'hidden') : (card.view = 'visible');
   if (selectOne == defaultCard) {
     card.selected = true;
     selectOne = card;
+    card.opacity = 0.5;
   } else if (selectTwo == defaultCard) {
     card.selected = true;
     selectTwo = card;
   }
   result = cards.value.filter((x) => x.selected == true);
+  if (selectOne == selectTwo) {
+    card.opacity = 1;
+    selectOne = defaultCard;
+    result[0].selected = false;
+    selectTwo = defaultCard;
+    result = [];
+  }
   if (result.length > 1) {
+    console.log(result);
+    if (result[0].id == result[1].id) {
+      console.log('UGOALI');
+      result[0].opacity = 1;
+      result[0].selected = false;
+      selectOne = defaultCard;
+      selectTwo = defaultCard;
+      result = [];
+      return;
+    }
     if (result[0].firstName == result[1].firstName) {
       cards.value.filter((x) => {
         if (result[0] == x || result[1] == x) {
@@ -33,10 +50,12 @@ const checkCard = (card: Card): void => {
       console.log(result.map((x) => console.log(x)));
       console.log('NO');
     }
-    cards.value.map((x) => (x.selected = false));
+    cards.value.map((x) => {
+      x.selected = false;
+      x.opacity = 1;
+    });
     selectOne = defaultCard;
     selectTwo = defaultCard;
-
     result = [];
   }
 };
@@ -46,7 +65,12 @@ const checkCard = (card: Card): void => {
   <section class="main">
     <div class="game">
       <div id="memory">
-        <div class="card" v-for="card in cards" :key="card.id" :style="{ visibility: card.view }">
+        <div
+          class="card"
+          v-for="card in cards"
+          :key="card.id"
+          :style="{ visibility: card.view, opacity: card.opacity }"
+        >
           <div class="value" @click="checkCard(card)">{{ card.firstName }}</div>
         </div>
       </div>
--- game/src/components/memoryGame/utility/cards.ts
+++ game/src/components/memoryGame/utility/cards.ts
@@ -6,27 +6,28 @@ export interface Card {
   view: StyleValue;
   bg: string;
   selected: boolean;
+  opacity: number;
 }
 
 const cards = ref<Card[]>([
-  { firstName: 'Frank', id: 1, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Frank', id: 2, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Vic', id: 3, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Vic', id: 4, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Gina', id: 5, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Gina', id: 6, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Jessi', id: 7, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Jessi', id: 8, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Erik', id: 9, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Erik', id: 10, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Man', id: 11, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Man', id: 12, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'MAttia', id: 13, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'MAttia', id: 14, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'FABBIO', id: 15, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'FABBIO', id: 16, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Jay', id: 17, view: 'visible', bg: 'blue', selected: false },
-  { firstName: 'Jay', id: 18, view: 'visible', bg: 'blue', selected: false },
+  { firstName: 'Frank', id: 1, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Frank', id: 2, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Vic', id: 3, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Vic', id: 4, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Gina', id: 5, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Gina', id: 6, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Jessi', id: 7, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Jessi', id: 8, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Erik', id: 9, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Erik', id: 10, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Man', id: 11, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Man', id: 12, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'MAttia', id: 13, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'MAttia', id: 14, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'FABBIO', id: 15, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'FABBIO', id: 16, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Jay', id: 17, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
+  { firstName: 'Jay', id: 18, view: 'visible', bg: 'blue', selected: false, opacity: 1 },
 ]);
 
 cards.value = cards.value.sort(() => Math.random() - 0.5);
