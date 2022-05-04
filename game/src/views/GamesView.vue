dcbac78df8c1507bbf3ee6a274da081bda461d60
kocierik <kocierik@gmail.com>
2022-05-04 14:11:11 +0200

    feat: add 2048

--- game/src/views/GamesView.vue
+++ game/src/views/GamesView.vue
@@ -28,5 +28,13 @@ import * as router from '@/router/index';
         HangMan
       </a>
     </router-link>
+    <router-link :to="router.GameDue">
+      <a
+        href="#"
+        class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+      >
+        2048
+      </a>
+    </router-link>
   </main>
 </template>
