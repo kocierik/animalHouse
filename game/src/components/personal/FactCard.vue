23a1345f670e6954ce7e5b694fa1cff22d399836
Mattia <mgiro2001@gmail.com>
2022-05-06 12:11:01 +0200

    feat: choose ur fighter

--- game/src/components/personal/FactCard.vue
+++ game/src/components/personal/FactCard.vue
@@ -1,27 +1,33 @@
 <template>
 <div class="content-center">
-  <a class="block max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
-    <div v-if=isLoading class="py=70">
-        <span> loading... </span>
+    <div v-if=isLoading class="flex justify-center">
+      <LoadingSpinner />
     </div>
-    <div v-else>
+  <a v-else class="block max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
       <div class="p-6">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Fact!</h5>
+        <div class="flex justify-start space-x-3">
+          <AnimalIcon class="mb-2 text-2xl font-bold tracking-tight" :animal=props.animal />
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Fact!</h5>
+        </div>
         <p class="font-normal text-gray-700">
           {{ fact }}
         </p>
-      </div>
+      <button @click="loadFact()" class="mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-20 rounded-full">
+        Another one! 
+      </button>
     </div>
 </a>
-<button @click="loadFact()" class="mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-40 rounded-full">
-  Button
-</button>
 </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onBeforeMount } from 'vue'
 import { fetchCatFacts } from "@/network/api"
+import LoadingSpinner from "@/components/common/LoadingSpinner.vue"
+import AnimalIcon from "@/components/common/AnimalIcon.vue"
+import { AnimalType } from 'shared'
+
+const props = defineProps<{animal : AnimalType}>()
 
 let fact = ref<string>("loading...")
 let isLoading = ref<boolean>(false)
--- game/src/views/PersonalView.vue
+++ game/src/views/PersonalView.vue
@@ -3,6 +3,19 @@ import ViewTitle from '@/components/common/ViewTitle.vue'
 import FactCard from '@/components/personal/FactCard.vue'
 import ImageCard from '@/components/personal/ImageCard.vue'
 import MusicCard from '@/components/personal/MusicCard.vue'
+import AddImageCard from '@/components/personal/AddImageCard.vue'
+import * as router from "@/router/index"
+import * as lsh from "@/helpers/localStoreHelper"
+import { AnimalType} from 'shared'
+import { ref } from 'vue'
+
+const animals = ref<AnimalType[]>()
+const a = localStorage.getItem(lsh.PersonalAnimals)
+if(a === null)
+  window.location.href = router.PersonalizeRoute
+else {
+  animals.value = JSON.parse(a);
+}
 </script>
 
 <template>
@@ -10,8 +23,11 @@ import MusicCard from '@/components/personal/MusicCard.vue'
     <ViewTitle 
       :title="'Personal'" 
       :background="'https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true'"/>
-    <FactCard />
-    <ImageCard />
-    <MusicCard />
+      <div class="md:flex md:flex-wrap">
+        <FactCard  v-for="a in animals" :animal="a" class="m-10"/>
+        <ImageCard v-for="a in animals" :animal="a" class="m-10"/>
+        <MusicCard v-for="a in animals" :animal="a" class="m-10"/>
+        <AddImageCard />
+    </div>
   </main>
 </template>
