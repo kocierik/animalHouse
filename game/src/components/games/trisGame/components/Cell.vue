55ffe329fcb6abbdc392616092c5df31d16cd2af
kocierik <kocierik@gmail.com>
2022-05-11 22:06:25 +0200

    feat: add ticTacToe game

---
+++ game/src/components/games/trisGame/components/Cell.vue
@@ -0,0 +1,50 @@
+<template>
+  <button @click="setCell()" class="flex">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+import { ref, watch } from 'vue'
+export default {
+  props: {
+    placement: Array,
+    label: String,
+  },
+  setup(props, { emit }) {
+    const place = ref(props.placement)
+    const label = ref(props.label)
+    watch(() => {
+      label.value = props.label
+    })
+    const setCell = () => {
+      emit('place', place.value)
+    }
+
+    return {
+      place,
+      // eslint-disable-next-line vue/no-dupe-keys
+      label,
+      setCell,
+    }
+  },
+}
+</script>
+
+<style scoped>
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4rem;
+  height: 4rem;
+  margin: 2px;
+  font-size: 2em;
+  font-weight: bold;
+  border: 1px solid #3c4042;
+  background: #606368;
+}
+button:focus {
+  outline: none;
+}
+</style>
---
+++ game/src/components/games/trisGame/components/board.ts
@@ -0,0 +1,49 @@
+/**
+ * board.js
+ * helper class to control the board
+ */
+
+// Game Grid [3x3]
+export default class Board {
+  cells: any
+  constructor(cells: any) {
+    this.cells = cells
+  }
+
+  has3InARow(player: string) {
+    for (let i = 0; i < 3; i++) {
+      // Horizontal
+      if (this.cells[0][i] === player && this.cells[1][i] === player && this.cells[2][i] === player) {
+        return true
+      }
+      // Vertical
+      if (this.cells[i][0] === player && this.cells[i][1] === player && this.cells[i][2] === player) {
+        return true
+      }
+    }
+    // Diagonals
+    if (this.cells[0][0] === player && this.cells[1][1] === player && this.cells[2][2] === player) {
+      return true
+    }
+    if (this.cells[2][0] === player && this.cells[1][1] === player && this.cells[0][2] === player) {
+      return true
+    }
+    return false
+  }
+
+  isGameOver() {
+    return this.getPossibleMoves().length === 0 || this.has3InARow('X') || this.has3InARow('O')
+  }
+
+  getPossibleMoves() {
+    const moves = []
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.cells[i][j] === '') {
+          moves.push({ x: i, y: j })
+        }
+      }
+    }
+    return moves
+  }
+}
