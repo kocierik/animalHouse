83a37db2b43ea8c8d2a10b6be51880c95e15d43c
Mattia <mgiro2001@gmail.com>
2022-05-22 13:10:54 +0200

    feat: auth serverside

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,7 +1,31 @@
 import User from '../entities/User'
 import { Request, Response } from 'express'
-import { JsonUserCreation, JsonLogin } from '../json/UserJson'
+import { JsonUserCreation, JsonLogin, JsonUser} from '../json/JsonUser'
+import {v4 as uuidv4} from 'uuid'
 import * as bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
+
+const SECRET = 'bigSecret'
+
+interface AuthData {
+  username: string,
+  guid: string
+}
+
+export const verifyToken = async (req: Request, res: Response, next: Function) => {
+  const authHeader = req.headers['authorization']
+  if (authHeader !== undefined) {
+    jwt.verify(authHeader, SECRET, (err, authData) => {
+      if (err)
+        res.sendStatus(403)
+      else {
+        req.authData = authData.authData
+        next()
+      }
+    })
+  } else
+    res.sendStatus(403)
+}
 
 export const registerPost = async (req: Request, res: Response) => {
   const userCreation = req.body as JsonUserCreation
@@ -24,6 +48,7 @@ export const registerPost = async (req: Request, res: Response) => {
     return res.status(400).send(`email ${userCreation.email} already taken`)
 
   const user = new User()
+  user.guid = uuidv4()
   user.username = userCreation.username
   user.email = userCreation.email
   user.password =  userCreation.password//bcrypt.hashSync(userCreation.password, 5)
@@ -40,7 +65,43 @@ export const loginPost = async (req: Request, res: Response) => {
 
   const hashed = login.password//bcrypt.hashSync(login.password, 5)
   const result = await User.find({'username' : login.username, 'password' : hashed})
-  if (result.length === 1) return res.send("login")
+  if (result.length === 1) {
+    const authData: AuthData = {
+      username: result[0].username,
+      guid: result[0].guid
+    }
+    const token = await jwt.sign({authData: authData}, SECRET);
+    return res.json({token})
+  }
   else return res.send("nope")
 }
+
+
+export const getUser = async (req, res) => {
+  const authGuid = req.authData.guid
+  const pathGuid = req.params.guid
+  if (pathGuid !== authGuid)
+    res.status(403).send( 'Can\'t access user with guid ' + pathGuid + ' (logged is ' + authGuid + ')')
+  else {
+    const result = await User.find({'username' : req.authData.username, 'guid': pathGuid})
+    if (result.length !== 1) {
+      res.status(400).send('Invalid guid ' + pathGuid)
+    }
+    const jsonUser = {
+      guid: result[0].guid,
+      username: result[0].username,
+      firstName: result[0].firstName,
+      lastName: result[0].lastName,
+      email: result[0].email,
+      phone: result[0].phone
+      // TODO more fields
+    }
+    res.json(jsonUser)
+  }
+}
+
+export const test = async (req, res) => {
+    res.json(req.authData)
+}
+
 
