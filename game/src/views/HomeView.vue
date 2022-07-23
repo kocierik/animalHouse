940642405a1921981833ec9731dd475423061f8e
kocierik <kocierik@gmail.com>
2022-07-23 14:29:42 +0200

    feat: add carousel

--- game/src/views/HomeView.vue
+++ game/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from '../components/common/Header.vue'
 import Footer from '../components/common/Footer.vue'
+import Carousel from '../components/common/carousel.vue'
 </script>
 <template>
   <Header />
   <div class="flex flex-1 flex-end">
     <div class="flex flex-1 flex-col flex-end">
+      <Carousel />
       <Footer />
     </div>
   </div>
