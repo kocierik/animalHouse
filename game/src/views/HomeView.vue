2cf8db955144bbaca599b4c58c364a375861f13d
kocierik <kocierik@gmail.com>
2022-07-25 12:25:48 +0200

    chore: add components

--- game/src/views/HomeView.vue
+++ game/src/views/HomeView.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import Header from '../components/common/Header.vue'
 import Footer from '../components/common/Footer.vue'
-import Carousel from '../components/common/carousel.vue'
+import Carousel from '../components/common/Carousel.vue'
+import SixCard from '../components/common/SixCard.vue'
+import Billing from '../components/common/Billing.vue'
 </script>
 <template>
   <Header />
   <div class="flex flex-1 flex-end">
     <div class="flex flex-1 flex-col flex-end">
       <div class="flex-1 pt-10 pb-10">
+        <SixCard />
         <Carousel />
+        <Billing />
       </div>
       <Footer />
     </div>
