6b836b068a67d64320ec3bd0e82275d95c6f7e1d
kocierik <kocierik@gmail.com>
2022-07-25 16:07:33 +0200

    feat: add product

---
+++ game/src/components/common/Shop.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import Product from './Product.vue'
+</script>
+<template>
+  <div x-data="{ cartOpen: false , isOpen: false }" class="bg-white">
+    <main class="my-8 text-center">
+      <h2 class="text-2xl leading-normal mb-2 font-bold text-black">Check some of our product</h2>
+      <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </main>
+  </div>
+</template>
--- game/src/views/HomeView.vue
+++ game/src/views/HomeView.vue
@@ -7,12 +7,14 @@ import Billing from '../components/common/Billing.vue'
 import TeamCards from '../components/common/TeamCards.vue'
 import Testimonials from '../components/common/Testimonials.vue'
 import NewsLetter from '../components/common/NewsLetter.vue'
+import Shop from '../components/common/Shop.vue'
 </script>
 <template>
   <Header />
   <div class="flex flex-1 flex-end">
     <div class="flex flex-1 flex-col flex-end">
       <div class="flex-1 pt-10 pb-10">
+        <Shop />
         <SixCard />
         <Carousel />
         <Testimonials />
