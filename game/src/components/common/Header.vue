041550379c7afe00021421b84be709d35179dd4d
kocierik <kocierik@gmail.com>
2022-07-25 12:25:41 +0200

    chore: maximaze header

--- game/src/components/common/Header.vue
+++ game/src/components/common/Header.vue
@@ -7,7 +7,7 @@
       style="
         background-position: 50%;
         background-image: url('https://www.amoreaquattrozampe.it/wp-content/uploads/2020/04/740cane_jack_russel_AdobeStock.jpeg');
-        height: 350px;
+        height: 95vh;
       "
     >
       <div
@@ -17,7 +17,7 @@
         <div class="flex justify-center items-center h-full">
           <div class="text-center text-white px-6 md:px-12">
             <h1 class="text-5xl font-bold mt-0 mb-6">The Best Resort</h1>
-            <h3 class="text-3xl font-bold mb-8">In The World</h3>
+            <h3 class="text-3xl font-bold mb-8">For your Pet</h3>
             <button
               type="button"
               class="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
