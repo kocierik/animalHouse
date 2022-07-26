1e11339e3eda37250d9b2f74828d8ee4d53ae74c
kocierik <kocierik@gmail.com>
2022-07-26 22:31:07 +0200

    chore: fix articles

--- front-office/src/views/common/Article.tsx
+++ front-office/src/views/common/Article.tsx
@@ -42,9 +42,10 @@ const products = [
 const Article = () => {
   return (
     <Link to="/product/">
-    <div className="bg-white w-full">
-      <div className="max-w-2xl mx-auto py-8 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className=" grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <div key={product.id} className="group relative">
               <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
