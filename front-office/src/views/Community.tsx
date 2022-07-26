9365b520c06beed1fda1071893c5077dd84340d5
kocierik <kocierik@gmail.com>
2022-07-26 14:57:15 +0200

    chore: format code

--- front-office/src/views/Community.tsx
+++ front-office/src/views/Community.tsx
@@ -22,7 +22,7 @@ const Community = () => {
                       Points
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                       Data
+                      Data
                     </th>
                     <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                       Game
