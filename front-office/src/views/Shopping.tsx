b2797fe3502eb9e69778d9071e65dddc60563f82
kocierik <kocierik@gmail.com>
2022-07-26 19:52:09 +0200

    chore: fix alignment

--- front-office/src/views/Shopping.tsx
+++ front-office/src/views/Shopping.tsx
@@ -8,8 +8,8 @@ const Shopping = () => {
       <Navbar />
       <div className="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
         <section className="bg-white py-8">
-          <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-            <nav id="store" className="w-full z-10 top-0 px-6 py-1">
+          <div className="container mx-auto justify-evenly	flex items-center flex-wrap pt-4 pb-12">
+            <nav id="store" className="w-full  z-10 top-0 px-6 py-1">
               <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
                 <a
                   className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
