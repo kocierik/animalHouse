f5923c418260f66740ecd260f60f600897edfc58
kocierik <kocierik@gmail.com>
2022-07-26 12:32:14 +0200

    feat: add navbar

--- front-office/src/index.tsx
+++ front-office/src/index.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './index.scss'
+import App from './App'
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
   document.getElementById('root')
-);
+)
