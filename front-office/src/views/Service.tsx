f689170415c8a20ba99f9069e0a64e9190c37be4
kocierik <kocierik@gmail.com>
2022-07-26 14:45:05 +0200

    feat: add leaderboard

---
+++ front-office/src/views/Service.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './common/Navbar'
+
+const Service = () => {
+  return (
+    <div>
+      <Navbar />
+      Service
+    </div>
+  )
+}
+
+export default Service
