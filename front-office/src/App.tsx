4677de57d2fe2b81caca5e837409666afb826a92
kocierik <kocierik@gmail.com>
2022-07-26 12:32:14 +0200

    feat: add navbar

--- front-office/src/App.tsx
+++ front-office/src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router';
-import Home from './Home';
+import React from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router'
+import Home from './views/Home'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
