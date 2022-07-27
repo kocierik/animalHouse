8050aa2ab6fe4dcf7abcec4cf087d236993ed0ec
kocierik <kocierik@gmail.com>
2022-07-27 12:07:05 +0200

    chore: lint code

--- game/src/network/api.ts
+++ game/src/network/api.ts
@@ -9,7 +9,7 @@ const _CAT_IMG_URL = 'https://api.thecatapi.com/v1/images/search'
 // Server api urls
 const _BASE_URL = 'http://localhost:8080/v1'
 const _AUTH = '/user/login'
-const _ANIMAL_CODES = "/animals/"
+const _ANIMAL_CODES = '/animals/'
 
 class GameApi extends Api {
   protected static getToken(): string {
@@ -25,4 +25,4 @@ export const fetchCatImg = async (): Promise<ApiResponse<CatImg>> => Api.get<Cat
 export const login = async (username: string, password: string) =>
   GameApi.post<any>(_BASE_URL + _AUTH, { username: username, password: password })
 
-export const getAnimalCode = async () => GameApi.get<{code: number, name: String}[]>(_BASE_URL +_ANIMAL_CODES)
+export const getAnimalCode = async () => GameApi.get<{ code: number; name: String }[]>(_BASE_URL + _ANIMAL_CODES)
