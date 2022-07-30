38cd4dcbb555ed0725d37cfb7db90f956b048794
kocierik <kocierik@gmail.com>
2022-07-30 12:27:09 +0200

    feat: add icon

--- front-office/src/views/common/feedComponents/CommentFeed.jsx
+++ front-office/src/views/common/feedComponents/CommentFeed.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import avatar from '../assets/imageprofile.jpg'
 import TimeAgo from 'timeago-react'
-// import { FaPencilAlt, FaHeart, FaTrash } from 'react-icons/fa'
+import { FaPencilAlt, FaHeart, FaTrash } from 'react-icons/fa'
 
 const CommentFeed = (props) => {
   const handleCommentLike = (cId) => {
@@ -17,11 +17,12 @@ const CommentFeed = (props) => {
       {props.comments.map((data) => {
         return (
           <>
+            {/* COLORE COMMENTI */}
             <div
               className={
                 data.id === props.comments[props.comments.length - 1].id
-                  ? 'flex items-center px-4 pb-4 rounded-b-lg bg-gray-100 space-x-4'
-                  : 'flex items-center px-4 pb-4 bg-gray-100 space-x-4'
+                  ? 'flex items-center px-4 pb-4 rounded-b-lg space-x-4'
+                  : 'flex items-center px-4 pb-4  space-x-4'
               }
             >
               <img
@@ -29,7 +30,7 @@ const CommentFeed = (props) => {
                 className="rounded-full flex-initial max-h-8 w-8 sm:max-h-10 sm:w-10"
                 alt="User profile"
               />
-              <div className="p-3 bg-periwinkle rounded-lg w-full">
+              <div className="p-5 bg-periwinkle rounded-lg w-full">
                 <div className="ml-2">
                   {/* Name & Time */}
                   <div className="flex w-full justify-between">
@@ -48,28 +49,28 @@ const CommentFeed = (props) => {
                   <p className="text-sm pt-1 pb-2">{data.content}</p>
 
                   {/* Bottom Bar */}
-                  <div className="flex items-center space-x-3 text-sm text-gray-500">
-                    <p>{data.isLiked ? data.likes + 1 : data.likes} Likes</p>
+                  <div className="flex items-center space-x-1 text-sm text-gray-500">
+                    <p>{data.isLiked ? data.likes + 1 : data.likes}</p>
                     <button
                       className={
                         data.isLiked
-                          ? 'flex items-center space-x-1 rounded-full bg-transparent text-red-500 duration-150'
-                          : 'flex items-center space-x-1 rounded-full bg-transparent text-gray-500 hover:text-red-500 duration-150'
+                          ? 'flex items-center p-1 space-x-1 rounded-full bg-transparent text-red-500 duration-150'
+                          : 'flex items-center p-1 space-x-1 rounded-full bg-transparent text-gray-500 hover:text-red-500 duration-150'
                       }
                       onClick={() => handleCommentLike(data.id)}
                     >
-                      {/* <FaHeart className='fill-current' /> */}
+                      <FaHeart className="fill-current" />
                       <p>{data.isLiked ? 'Liked' : 'Like'}</p>
                     </button>
-                    <button className="flex items-center space-x-1 rounded-full bg-transparent text-gray-500">
-                      {/* <FaPencilAlt className='fill-current' /> */}
+                    <button className="flex items-center p-1 space-x-1 rounded-full bg-transparent text-gray-500">
+                      <FaPencilAlt className="fill-current" />
                       <p>Edit</p>
                     </button>
                     <button
-                      className="flex items-center space-x-1 rounded-full bg-transparent text-gray-500 hover:text-black duration-150"
+                      className="flex items-center p-1 space-x-1 rounded-full bg-transparent text-gray-500 hover:text-black duration-150"
                       onClick={() => handleCommentDelete(data.id)}
                     >
-                      {/* <FaTrash className='fill-current' /> */}
+                      <FaTrash className="fill-current" />
                       <p>Delete</p>
                     </button>
                   </div>
