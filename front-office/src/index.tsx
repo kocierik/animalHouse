a41889477bd32954b0a54781f0eac3cd2a6d3e9a
kocierik <kocierik@gmail.com>
2022-07-30 12:57:03 +0200

    feat: change to react18

--- front-office/src/index.tsx
+++ front-office/src/index.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.scss'
 import App from './App'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById('root')
-)
+);
