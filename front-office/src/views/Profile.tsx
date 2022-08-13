e50927d7977ef45c8394076d02c4fa22a0525798
kocierik <kocierik@gmail.com>
2022-08-13 12:41:25 +0200

    feat: fix profile p

--- front-office/src/views/Profile.tsx
+++ front-office/src/views/Profile.tsx
@@ -62,7 +62,7 @@ const Profile = () => {
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                    <div className="py-6 px-3 mt-32 sm:mt-0">
+                    <div className="py-6 px-3 mt-32 sm:mt-0 text-center	">
                       <button
                         className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
                         type="button"
