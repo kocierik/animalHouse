9c516ae2e2f6df04a42259bf5c02ad27fdf1b8dd
kocierik <kocierik@gmail.com>
2022-08-13 12:34:06 +0200

    feat: change post card

--- front-office/src/views/common/feedComponents/PostCard.jsx
+++ front-office/src/views/common/feedComponents/PostCard.jsx
@@ -25,20 +25,29 @@ const PostCard = (props) => {
       {/* Card Base */}
       <div className="flex-row   container max-w-md sm:max-w-xl mb-14 h-auto p-2 rounded-lg shadow-md divide-y divide-solid bg-gray-50">
         {/* Top Half - Avatar & Text Box */}
-        <div className="flex content-start p-4">
-          <img
-            src={avatar}
-            className="rounded-full flex-initial max-h-12 w-12 sm:max-h-14 sm:w-14 mb-8 duration-150"
-            alt="User profile"
-          />
-          <textarea
-            id="post-input"
-            rows={3}
-            placeholder="What is on your pet mind?"
-            value={inputText}
-            className="flex-grow rounded font-sans placeholder-gray-400 self-start ml-3 sm:ml-4 mt-2 sm:mt-3 text-sm sm:text-base focus:outline-none"
-            onChange={onChange}
-          />
+        <div className="flex flex-col	 content-start p-4">
+          <div className="flex mb-8">
+            <span className="">
+              <img
+                src={avatar}
+                className="rounded-full flex-initial max-h-12 w-12 sm:max-h-14 sm:w-14  duration-150"
+                alt="User profile"
+              />
+            </span>
+            <div className="flex flex-1 px-5 items-center">
+              <span className="font-black	text-lg	">Erik Koci</span>
+            </div>
+          </div>
+          <div className="flex">
+            <textarea
+              id="post-input"
+              rows={3}
+              placeholder="What is on your pet mind?"
+              value={inputText}
+              className="flex-grow rounded font-sans placeholder-gray-400 self-start ml-3 sm:ml-4 mt-2 sm:mt-3 text-sm sm:text-base focus:outline-none"
+              onChange={onChange}
+            />
+          </div>
         </div>
 
         {/* Lower Half - Photo/Video & Post Buttons */}
