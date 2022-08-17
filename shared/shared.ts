a59e584389ac3edd58dd1bfc94e124b623f39168
kocierik <kocierik@gmail.com>
2022-08-17 12:53:15 +0200

    feat: add retrive record 2038

--- shared/shared.ts
+++ shared/shared.ts
@@ -1,3 +1,3 @@
 export { AnimalType, getAnimalPicture } from "./animalPics";
 export { Api, ApiResponse } from './api'
-export { login, getAnimalCode } from './apiRepository'
+export { login, getAnimalCode, putUserScore } from './apiRepository'
