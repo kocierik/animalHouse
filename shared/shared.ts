d71819664705db8e1ad29524da3cefaa4de9f549
kocierik <kocierik@gmail.com>
2022-08-18 12:59:40 +0200

    merge: branch main

--- shared/shared.ts
+++ shared/shared.ts
@@ -1,3 +1,4 @@
-export { AnimalType, getAnimalPicture } from "./animalPics";
-export { Api, ApiResponse } from './api'
-export { login, getAnimalCode } from './apiRepository'
+export { AnimalType, getAnimalPicture } from "./src/animalPics";
+export { Api, ApiResponse } from './src/api'
+export { login, getAnimalCode, getUserScore } from './src/apiRepository'
+export * as LoginHelper from './src/loginHelper'
