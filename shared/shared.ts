5f2a2c709acd5d01e32c628109956b70ffa33506
kocierik <kocierik@gmail.com>
2022-08-19 12:20:20 +0200

    feat: import interface

--- shared/shared.ts
+++ shared/shared.ts
@@ -1,6 +1,7 @@
 export { AnimalType, getAnimalPicture } from "./src/animalPics";
 export { Api, ApiResponse } from './src/api'
-export { login, getAnimalCode, getUserScore } from './src/apiRepository'
+export { login, getAnimalCode, getUserScore, getMarketProduct } from './src/apiRepository'
 export * as LoginHelper from './src/loginHelper'
 export * as Community from "./src/json/Community"
+export * as ProductMarked from "./src/json/ProductMarked"
 export * as ApiRepository from './src/apiRepository'
