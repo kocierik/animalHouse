8280d7fb985265c1aa76858ffed4d311e74900e8
kocierik <kocierik@gmail.com>
2022-08-19 12:19:33 +0200

    chore: add API marketProduct

--- shared/src/apiRepository.ts
+++ shared/src/apiRepository.ts
@@ -1,5 +1,6 @@
 import { Api } from './api'
 import type { IGameValues } from './json/Community';
+import type { IProductMarked } from './json/Articles';
 
 // Server api urls
 const _BASE_URL = 'http://localhost:8080/v1'
@@ -8,6 +9,7 @@ const _ANIMAL_CODES = '/animals/'
 const _REGISTER_CODES = '/register/'
 const _SCORE_CODES = '/user/:id/score/' 
 const _LEADERBOARD_CODES = '/community/game/scoreboard' 
+const _MARKET_PRODUCT_CODES = '/market/product/' 
 // http://localhost:8080/v1/user/register 
 
 export const login = async (username: string, password: string) =>
@@ -17,5 +19,6 @@ export const getAnimalCode = async () => Api.get<{ code: number; name: String }[
 export const postUserRegister = async () => Api.post<{ code: number;  name: String }[]>(_BASE_URL + _REGISTER_CODES,  "")
 export const putUserScore = async () => Api.post<{ code: number;  name: String }[]>(_BASE_URL + _SCORE_CODES,  "")
 export const getUserScore = async () => Api.get<IGameValues[]>(_BASE_URL + _LEADERBOARD_CODES)
+export const getMarketProduct = async () => Api.get<IProductMarked[]>(_BASE_URL + _MARKET_PRODUCT_CODES)
 
 // TODO insert here other calls!!!!
