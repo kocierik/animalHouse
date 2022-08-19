1bd48ef98d65636a79b2586c212f02bee4ce865a
kocierik <kocierik@gmail.com>
2022-08-19 11:19:03 +0200

    chore: import shared func

--- shared/shared.ts
+++ shared/shared.ts
@@ -2,3 +2,5 @@ export { AnimalType, getAnimalPicture } from "./src/animalPics";
 export { Api, ApiResponse } from './src/api'
 export { login, getAnimalCode, getUserScore } from './src/apiRepository'
 export * as LoginHelper from './src/loginHelper'
+export * as Community from "./src/json/Community"
+export * as ApiRepository from './src/apiRepository'
