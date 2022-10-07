f0d7f93c5fc7bd40d56544eb9ffd0c27440fd93d
kocierik <kocierik@gmail.com>
2022-10-07 23:08:51 +0200

    feat: add game constant id

--- shared/shared.ts
+++ shared/shared.ts
@@ -3,6 +3,7 @@ export { Api, ApiResponse } from './src/api'
 export * as LocalStorageHelper from './src/animalPics'
 export * as Helpers from './src/helpers'
 export * as ApiRepository from './src/apiRepository'
+export * as GameConstant from "./src/gameConstant"
 export * as JsonUser from './src/json/user'
 export * as JsonAnimal from './src/json/animal'
 export * as Community from "./src/json/Community"
