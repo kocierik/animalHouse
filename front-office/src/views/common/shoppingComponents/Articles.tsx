791b92707e27307c72a9c9615177aecb5ee5bcf0
kocierik <kocierik@gmail.com>
2022-10-08 12:09:57 +0200

    chore: fix scale image

--- front-office/src/views/common/shoppingComponents/Articles.tsx
+++ front-office/src/views/common/shoppingComponents/Articles.tsx
@@ -58,14 +58,14 @@ const Articles = () => {
     <Link to="/product/">
       <div className="bg-white">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          <div className="mt-6 grid grid-cols-1 gap-y-20 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {article?.map((product) => (
               <div key={product._id} className="group relative">
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                   <img
                     src={product.image}
                     alt={product.name}
-                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                    className="w-full h-full object-center object-unset lg:w-full lg:h-full"
                   />
                 </div>
                 <div className="mt-4 flex justify-between">
