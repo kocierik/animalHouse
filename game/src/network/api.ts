cb44c27f571b320a48ac4a6825b30493b4a14629
kocierik <kocierik@gmail.com>
2022-10-08 11:23:57 +0200

    feat: add horoscope

--- game/src/network/api.ts
+++ game/src/network/api.ts
@@ -1,10 +1,13 @@
 import { Api, ApiResponse } from 'shared'
 import type { CatFact, CatImg } from './models/CatFact'
-
+import type { IHoroscope } from './models/Horoscope'
 // Various api urls
 const _CAT_FACT_URL = ' https://meowfacts.herokuapp.com/'
 const _CAT_IMG_URL = 'https://api.thecatapi.com/v1/images/search'
+const HOROSCOPE_URL = 'http://sandipbgt.com/theastrologer/api/horoscope/aries/today/'
 
 export const fetchCatFacts = async (): Promise<ApiResponse<CatFact>> => Api.get<CatFact>(_CAT_FACT_URL)
 
 export const fetchCatImg = async (): Promise<ApiResponse<CatImg>> => Api.get<CatImg>(_CAT_IMG_URL)
+
+export const fetchHoroscope = async (): Promise<ApiResponse<IHoroscope>> => Api.get<IHoroscope>(HOROSCOPE_URL)
