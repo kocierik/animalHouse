42ebcc7b434739ba89221efc67fec2b7f61824e2
kocierik <kocierik@gmail.com>
2022-10-09 16:02:13 +0200

    chore: fix import

--- game/src/network/api.ts
+++ game/src/network/api.ts
@@ -1,7 +1,7 @@
 import { Api, ApiResponse } from 'shared'
 import type { CatFact, CatImg } from './models/CatFact'
 import type { IHoroscope } from './models/Horoscope'
-import { stringFormat } from '../../../shared/src/helpers'
+import { Helpers } from 'shared'
 // Various api urls
 const _CAT_FACT_URL = ' https://meowfacts.herokuapp.com/'
 const _CAT_IMG_URL = 'https://api.thecatapi.com/v1/images/search'
@@ -12,4 +12,4 @@ export const fetchCatFacts = async (): Promise<ApiResponse<CatFact>> => Api.get<
 export const fetchCatImg = async (): Promise<ApiResponse<CatImg>> => Api.get<CatImg>(_CAT_IMG_URL)
 
 export const fetchHoroscope = async (sign: string): Promise<ApiResponse<IHoroscope>> =>
-  Api.get<IHoroscope>(stringFormat(HOROSCOPE_URL, sign))
+  Api.get<IHoroscope>(Helpers.stringFormat(HOROSCOPE_URL, sign))
