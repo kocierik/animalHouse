51369e7365fa4b648fdd284463366fea4088eb67
kocierik <kocierik@gmail.com>
2022-10-09 23:27:22 +0200

    feat: add transition scroll

--- front-office/src/views/common/shoppingComponents/Articles.tsx
+++ front-office/src/views/common/shoppingComponents/Articles.tsx
@@ -56,7 +56,7 @@ const Articles = () => {
 
   return (
     <Link to="/product/">
-      <div className="bg-white">
+      <div className="bg-white" data-aos="zoom-in" data-aos-duration="1500">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {article?.map((product) => (
