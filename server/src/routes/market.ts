e1bb58820ae2c87cb9c089a1780dcc5821eb1a18
kocierik <kocierik@gmail.com>
2022-10-17 23:00:05 +0200

    chore: rename params

--- server/src/routes/market.ts
+++ server/src/routes/market.ts
@@ -1,4 +1,4 @@
-import { STATUS_OK, STATUS_BAD_REQUEST } from '../const'
+import { STATUS_OK, STATUS_BAD_REQUEST } from '../const';
 import JsonError from '../json/JsonError'
 import { Request, Response } from 'express'
 import Product from '../entities/Product'
@@ -9,11 +9,12 @@ export const getProducts = async (_: Request, res: Response) => {
 
 export const getProduct = async (req: Request, res: Response) => {
   const pathId = req.params.id
-    if (req.query.id) {
+    if (pathId) {
       // Check if a product with that id exists
-      if (await Product.exists({ _id: req.query.id }))
+      if (await Product.exists({ _id: pathId }))
         return res.status(STATUS_OK).json(await Product.findById( pathId ))
-      else return res.status(STATUS_BAD_REQUEST).json(new JsonError(`Invalid product id ${req.query.id}`))
+      else return res.status(STATUS_BAD_REQUEST).json(new JsonError(`Invalid product id ${pathId}`))
+    } else {
+      return res.status(STATUS_BAD_REQUEST).json(new JsonError(`Invalid product id ${pathId}`))
     }
-    return res.status(STATUS_OK).json(await Product.findById( pathId ))
 }
