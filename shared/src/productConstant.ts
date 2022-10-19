99a40735f04efb2a60530dd0a5d2fe8b5f7cde3e
kocierik <kocierik@gmail.com>
2022-10-20 00:18:31 +0200

    feat: add constant product type

---
+++ shared/src/productConstant.ts
@@ -0,0 +1,7 @@
+export const WEARING = '62f3c0540ac73a2bc4764da7'
+export const FOOD = '62f3c0540ac73a2bc4764da8'
+export const HEALTH = '62f3c0540ac73a2bc4764da9'
+export const ACCESSORIES = '62f3c0540ac73a2bc4764daa'
+export const PUPPIES = '62f3c0540ac73a2bc4764dab'
+export const ENTERTAINMENT = '62f3c0540ac73a2bc4764dac'
+export const BEAUTY = '62f3c0540ac73a2bc4764dad'
