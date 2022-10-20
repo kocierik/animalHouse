f996e08e9f8e9b5c9546b827eb58c8c0d3be22a4
Gabriele Crestanello <gabricre01@gmail.com>
2022-10-20 12:02:29 +0200

    fix: swagger /user to /users + minor changes to chart

--- back-office/public/js/chart.js
+++ back-office/public/js/chart.js
@@ -15,17 +15,13 @@ new Chartist.Line('#chart1', {
     showLine: true,
     showPoint: true,
     lineSmooth: true,
-    low: 0
+    low: 0,
+    onlyInteger: true
 });
 
 
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object. As a third parameter we pass in our custom options.
 new Chartist.Line('#chart2', {
-    // A labels array that can contain any sort of values
     labels: ['1/10', '2/10', '3/10', '4/10', '5/10', '6/10', '7/10'],
-    // Our series array that contains series objects or in this case series data arrays
     series: [
         [34811, 0, 34103, 22039, 0, 3491, 12390]
     ]
