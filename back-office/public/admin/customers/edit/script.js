21b5d17e3115f43fcf0c2015fba3c041bba81d8c
Gabriele Crestanello <gabricre01@gmail.com>
2022-10-21 09:07:38 +0200

    style: minor stuff

---
+++ back-office/public/admin/customers/edit/script.js
@@ -0,0 +1,78 @@
+//https://stackoverflow.com/questions/19491336/how-to-get-url-parameter-using-jquery-or-plain-javascript
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+function retrieveUser(id) {
+    var url = "/v1/users/" + id;
+    fetch(url, {
+
+    }).then((response) => response.json()).then((data) => {
+        data.forEach(function (el) {
+            console.log(el);
+            $("#grid-prod-name").val(el.firstName);
+            $("#grid-price").val(el.price);
+            $("#grid-category").val(el.categoryId);
+            $("#grid-description").val(el.username);
+            //$("#grid-quantity").val(el.stock); TODO
+            $("#imgPlaceholder").attr("src", el.image);
+
+        });
+    });
+}
+
+$(document).ready(function () {
+    var id = getUrlParameter('id');
+    if (id) {
+        retrieveUser(id);
+    }
+});
+
+//TODO FIX SOME ERRS ON ASYNC RESPONSE
+
+$("#addProduct").click(function () {
+    var img = $('#grid-image').prop('files')[0];
+    if (img) {
+
+
+        var headers = {
+            "Content-Type": "application/json",
+            "Access-Control-Origin": "*"
+        }
+        var data = {
+            "name": $("#grid-prod-name").val(),
+            "price": $("#grid-price").val(),
+            "categoryId": $("#grid-category").val(),
+            "description": $("#grid-description").val(),
+            "animalTargets": [],
+            "image": "/public/products/" + getUrlParameter('id') + ".jpg"
+        };
+        console.log(data);
+        /*fetch("/v1/market/products", {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(data)
+        }).then((response) => response.json()).then(data => {
+            //console.log(data);
+            window.location.assign("../");
+            //$("#productPostResult").html("<h3>Here is the product you added</h3>");
+            //$("#productPostResult").append([{ displaySize: '-md-12', img: data.image, name: data.name, price: data.price, id: data._id }].map(Item));
+            //retrieveProduct('#productDeleteItems', "");
+        }).catch(function () {
+            alert("ERRORE");
+        });
+        */
+    }
+});
--- back-office/public/admin/customers/script.js
+++ back-office/public/admin/customers/script.js
@@ -8,14 +8,14 @@ function retrieveUsers(target) {
         $(target).text("");
 
         data.forEach(function (el) {
-            $(target).append([{ username: el.username, firstName: el.firstName, lastName: el.lastName, id: el.id, email: el.email, phone: el.phone, address: el.address }].map(Item));
+            $(target).append([{ username: el.username, firstName: el.firstName, lastName: el.lastName, id: el._id, email: el.email, phone: el.phone, address: el.address }].map(Item));
         });
     });
 }
-function itemRemove(id) {
+function userRemove(id) {
     if (confirm('Are you sure you want to remove the product ' + id + '?')) {
         $.ajax({
-            url: "/v1/market/products/" + id,
+            url: "/v1/users/" + id,
             type: 'DELETE',
             success: function (result) {
                 //alert(result);
@@ -30,14 +30,21 @@ const Item = ({ username, firstName, lastName, id, email, phone, address }) => `
 <tr>
     <td class="p-2 py-8 border-b border-solid border-gray-300">
         <div class="pl-4 flex flex-wrap flex-row items-center">
-            
-            <div class="mr-4 h-16 w-64 block flex flex-row items-center text-gray-700">${username}</div>
-            <div class="mr-4 h-16 w-64 block flex flex-row items-center text-gray-700">${firstName}</div>
-            <div class="mr-4 h-16 w-64 block flex flex-row items-center text-gray-700">${lastName}</div>
+            <div class="mr-4 h-16 w-16 block flex flex-row items-center">
+                <img class="rounded-lg" src="/favicon.ico"></div>
+            <div class="mr-4 h-16 w-48 block flex flex-row items-center text-gray-700">${username}</div>
+            <div class="mr-4 h-16 w-48 block flex flex-row items-center text-gray-700">${firstName}</div>
+            <div class="mr-4 h-16 w-48 block flex flex-row items-center text-gray-700">${lastName}</div>
+            <div class="mr-4 h-16 w-48 block flex flex-row items-center text-gray-700">${email}</div>
+            <div class="mr-4 h-16 w-48 block flex flex-row items-center text-gray-700">${phone}</div>
+            <div class="mr-4 h-16 w-64 block flex flex-row items-center text-gray-700">${address}</div>
+
         </div>
 
     </td>
     <td class="text-right p-2 pr-4 border-b border-solid border-gray-300 text-gray-700">
+        <a href="view/?id=${id}"><i class="bi bi-eye-fill"></i></a>
+        &nbsp;&nbsp;
         <a href="edit/?id=${id}"><i class="bi bi-pencil"></i></a>
         &nbsp;&nbsp;
         <button onclick=itemRemove("${id}")><i class="bi bi-trash"></i></button>
