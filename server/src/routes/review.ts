d8cea1d516369a8f45ba0117cb4c99fd1686aab7
kocierik <kocierik@gmail.com>
2022-10-22 15:02:18 +0200

    feat: write post review

--- server/src/routes/review.ts
+++ server/src/routes/review.ts
@@ -1,9 +1,8 @@
 import { STATUS_OK, STATUS_BAD_REQUEST, STATUS_NOT_FOUND } from '../const'
 import JsonError from '../json/JsonError'
 import { Request, Response } from 'express'
-import Product from '../entities/Product'
-import { JsonReview } from '../json/JsonReview'
 import Review from '../entities/Review'
+import { JsonReview } from '../json/JsonReview';
 
 
 export const getReviews = async (req: Request, res: Response) => {
@@ -13,6 +12,33 @@ export const getReviews = async (req: Request, res: Response) => {
     if(res.status(STATUS_OK).json(await Review.find({productId: pathId}))){
       return res.status(STATUS_OK).json(await Review.find({productId: pathId}))
     } else return res.status(STATUS_BAD_REQUEST).json(new JsonError(`Invalid  product id ${pathId}`))
+  } else {
+    return res.status(STATUS_BAD_REQUEST).json(new JsonError(`Url Product id doesn't exist ${pathId}`))
+  }
+}
+
+export const postReview = async (req: Request, res: Response) => {
+  const pathId = req.params.id
+  const reviewCreation = req.body as JsonReview
+  
+  // Check existing product
+  if (pathId) {
+    if(!reviewCreation.star)
+      return res.status(STATUS_BAD_REQUEST).json(new JsonError(`insert a valid number of star`))
+    if(reviewCreation.star){
+      const review = new Review()
+      review.username = reviewCreation.username
+      review.productId = reviewCreation.productId
+      review.comment = reviewCreation.comment
+      review.star = reviewCreation.star
+      review.date = reviewCreation.date
+      try {
+        await review.save()
+      } catch (error) {
+        return res.status(STATUS_BAD_REQUEST).json(new JsonError(`Error on creation review: ${error}`))
+      }
+      return res.status(STATUS_OK).json(review)
+    } 
   } else {
     return res.status(STATUS_BAD_REQUEST).json(new JsonError(`Url Product id doesn't exist ${pathId}`))
   }
