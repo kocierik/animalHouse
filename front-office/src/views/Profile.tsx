25197f40dd365420bc1e48036a17b98788199a5f
kocierik <kocierik@gmail.com>
2022-10-25 16:24:43 +0200

    chore: change position

--- front-office/src/views/Profile.tsx
+++ front-office/src/views/Profile.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { ApiRepository, type JsonUser  } from 'shared';
+import { ApiRepository, type JsonUser, Helpers } from 'shared';
 import { validate } from 'uuid';
+import { isLogged } from '../../../shared/src/helpers';
 
 
 const Profile = () => {
@@ -75,13 +76,15 @@ const Profile = () => {
                     </div>
                   </div>
              
+
                 
                 </div>
                 <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">{user?.username} </h3>
+                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">{test?.username} </h3>
                   <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
                     <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i> test
                   </div>
+                  
                   <div className="mb-2 text-gray-700 mt-10">
                     <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
                     Solution Manager - Creative Tim Officer
@@ -91,6 +94,39 @@ const Profile = () => {
                     University of Computer Science
                   </div>
                 </div>
+                
+                <div className='flex flex-col items-center'>
+                  <div className="w-full  lg:w-4/12 px-4 lg:order-1">
+                    <div className="flex flex-col justify-center py-4 lg:pt-4 pt-8">
+                      <div className='flex flex-row justify-center items-center'>
+                      <div className="mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">22</span>
+                        <span className="text-sm text-gray-500">Friends</span>
+                      </div>
+                      <div className="mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">10</span>
+                        <span className="text-sm text-gray-500">Photos</span>
+                      </div>
+                      </div>
+                  { Helpers.isLogged() &&  <div className="w-full lg:w-4/12  lg:order-3 lg:text-right lg:self-center">
+                    <div className="py-6 px-3 mt-32 sm:mt-0 text-center	">
+                      <button
+                        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
+                        type="button"
+                        style={{ transition: 'all .15s ease' }}
+                      >
+                        Connect
+                      </button>
+                    </div>
+                  </div>}
+                      {/* <div className="lg:mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">89</span>
+                        <span className="text-sm text-gray-500">Comments</span>
+                      </div> */}
+                    </div>
+                  </div>
+                </div>
+
                 <div className="mt-10 py-10 border-t border-gray-300 text-center">
                   <div className="flex flex-wrap justify-center">
                     <div className="w-full lg:w-9/12 px-4">
