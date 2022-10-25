d72567699136bbf6230c7853833204bd3a230d86
kocierik <kocierik@gmail.com>
2022-10-25 15:48:36 +0200

    feat: add user info

--- front-office/src/views/Profile.tsx
+++ front-office/src/views/Profile.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ApiRepository } from 'shared';
 
-// Temporaneo
-const user = [
-  { username: "fuck", address: { city: "fuck" } }
-]
+interface IUser {
+  username: string
+}
+
 
 const Profile = () => {
+
+  const [user,setUser] = useState<IUser>()
+  
+  const getUserInfo = async () =>{
+    const val = (await ApiRepository.getCurrentUser()).data
+    setUser(val!)
+  }
+  
+  useEffect(()=>{
+    getUserInfo()
+  },[])
+
   return (
     <>
       <main className="profile-page" data-aos="zoom-in" data-aos-duration="1500">
-        <section className="relative block" style={{ height: '500px' }}>
+        <section className="relative block" style={{ height: '400px' }}>
           <div
             className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
@@ -51,38 +64,13 @@ const Profile = () => {
                       />
                     </div>
                   </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                    <div className="py-6 px-3 mt-32 sm:mt-0 text-center	">
-                      <button
-                        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
-                        type="button"
-                        style={{ transition: 'all .15s ease' }}
-                      >
-                        Connect
-                      </button>
-                    </div>
-                  </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                    <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                      <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">22</span>
-                        <span className="text-sm text-gray-500">Friends</span>
-                      </div>
-                      <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">10</span>
-                        <span className="text-sm text-gray-500">Photos</span>
-                      </div>
-                      {/* <div className="lg:mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">89</span>
-                        <span className="text-sm text-gray-500">Comments</span>
-                      </div> */}
-                    </div>
-                  </div>
+             
+                
                 </div>
                 <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">{user[0].username} </h3>
+                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">{user?.username} </h3>
                   <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
-                    <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i> {user[0].address.city}
+                    <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i> test
                   </div>
                   <div className="mb-2 text-gray-700 mt-10">
                     <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
