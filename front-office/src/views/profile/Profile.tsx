b0537e983533fe1e6df36d9c3f5e107724f2bd3d
kocierik <kocierik@gmail.com>
2022-10-28 23:40:47 +0200

    chore: add post func

--- front-office/src/views/profile/Profile.tsx
+++ front-office/src/views/profile/Profile.tsx
@@ -5,7 +5,8 @@ import { ApiRepository, type JsonUser, Helpers } from 'shared';
 const Profile = () => {
 
   const [user,setUser] = useState<JsonUser.JsonUser>()
-  
+  const [file, setFile] = useState<File>()
+
   const test = {
     email: "man@ah.com",
     username: "erikMan",
@@ -17,16 +18,22 @@ const Profile = () => {
     address: {cap: 3434, city: "rimini",contry: "riccione", street:"via annibolina"}
   }
 
-
-  const getUserInfo = async () =>{
-    const val = (await ApiRepository.getCurrentUser()).data
-    //setUser(val!)
-    
+  const sendImage = async () =>{
+    if(file){
+      const resp = await ApiRepository.postUserPicture(Helpers.getUserId(), file!) 
+      if (resp.esit)
+        console.log("GHESBORO")
+      else
+        console.log("Nope, sorry :(")
+    }
   }
+  useEffect(() =>{
+    console.log(file)
+    sendImage()
+  },[file])
+
+
   
-  useEffect(()=>{
-    getUserInfo()
-  },[])
 
   return (
     <>
@@ -64,13 +71,16 @@ const Profile = () => {
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                    <div className="relative">
-                      <img
-                        alt="..."
-                        src="imageProfile.png"
-                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
-                        style={{ maxWidth: '150px' }}
-                      />
+                    <div className="relative -mt-20 w-30 h-24 flex  justify-center">
+                       <img className=" rounded-full -mt-5 border border-gray-100 shadow-sm" src="https://i1.wp.com/www.cinefilos.it/wp-content/uploads/2017/01/Avatar-Sequel.jpg" alt="user image" />
+                    <div className="absolute top-0 right-0 h-6 w-6 my-1  border-2 border-white rounded-full bg-gray-300 z-2">
+                      <label>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none"  viewBox="0 0 24 24" strokeWidth="2.5" stroke="black" className="w-6 h-6 cursor-pointer ">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                      </svg>
+                      <input id="dropzone-file" type="file" className="hidden" onChange={(e) =>setFile( e.target.files![0]) } />
+                      </label>
+                    </div>
                     </div>
                   </div>
              
@@ -80,7 +90,7 @@ const Profile = () => {
                 <div className="text-center mt-12">
                   <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">{test?.username} </h3>
                   <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
-                    <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i> test
+                    <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i> {test.firstName} {test.lastName}
                   </div>
                   
                   <div className="mb-2 text-gray-700 mt-10">
@@ -98,15 +108,11 @@ const Profile = () => {
                     <div className="flex flex-col justify-center py-4 lg:pt-4 pt-8">
                       <div className='flex flex-row justify-center items-center'>
                       <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">22</span>
-                        <span className="text-sm text-gray-500">Friends</span>
-                      </div>
-                      <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">10</span>
-                        <span className="text-sm text-gray-500">Photos</span>
+                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">{test.animals.length}</span>
+                        <span className="text-sm text-gray-500">Animals</span>
                       </div>
                       </div>
-                  { Helpers.isLogged() &&  <div className="w-full lg:w-4/12  lg:order-3 lg:text-right lg:self-center">
+                  {/* { Helpers.isLogged() &&   <div className="w-full lg:w-4/12  lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0 text-center	">
                       <button
                         className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
@@ -116,7 +122,7 @@ const Profile = () => {
                         Connect
                       </button>
                     </div>
-                  </div>}
+                  </div>} */}
                       {/* <div className="lg:mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">89</span>
                         <span className="text-sm text-gray-500">Comments</span>
