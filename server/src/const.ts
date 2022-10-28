1556e98c670f776373c0a86f6a5f965c2190fc1a
kocierik <kocierik@gmail.com>
2022-10-28 23:40:31 +0200

    chore: add field

--- server/src/const.ts
+++ server/src/const.ts
@@ -12,6 +12,7 @@ export const BACKOFFICE_DIR = '/../../back-office/public'
 
 export const SERVER_PORT = 8080
 export const CURR_API_VERSION = '/v1'
+export const PICTURE_DIR = "/../pictures"
 
 export const DB_NAME = 'animal-house-db'
 export const DB_ADDR = '127.0.0.1'
