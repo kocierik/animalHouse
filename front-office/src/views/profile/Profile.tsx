4596e12020b81173bf0a1d425ae33ca5e913c679
kocierik <kocierik@gmail.com>
2022-10-29 13:28:19 +0200

    chore: add flex-col

--- front-office/src/views/profile/Profile.tsx
+++ front-office/src/views/profile/Profile.tsx
@@ -109,7 +109,7 @@ const Profile = () => {
                   <div className="w-full   px-4 lg:order-1">
                     <div className="flex flex-col justify-center py-4 lg:pt-4 pt-8">
                       <div className='flex flex-row justify-center items-center'>
-                      <div className="mr-4 p-3 text-center flex justify-center flex-1 gap-5 flex-col md:flex-row">
+                      <div className="mr-4 p-3 text-center flex justify-center flex-1 gap-5 md:flex-row">
                         <AnimalCard />
                         <AnimalCard />
                       </div>
