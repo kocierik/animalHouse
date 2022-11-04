9deac373a02b39f9a71742974239d5c42efeabd5
mattia <m@tt.ia>
2022-11-04 23:41:42 +0100

    feat: swagger cleanup

--- server/src/json/JsonAnimal.ts
+++ server/src/json/JsonAnimal.ts
@@ -1,23 +1,28 @@
-/**
-* @swagger
-*  Animal:
-*    type: object
-*    properties:
-*      _id:
-*        type: string
-*      name:
-*        type: string
-*      type:
-*        type: string
-*      userId:
-*        type: string
-*      age:
-*        type: number
-* */
 export interface JsonAnimal {
   _id: string
   name: string
   age: number
   type: string
   userId: string
+}
+
+export const SwaggerAnimal = {
+  type: "object",
+  properties: {
+    _id: {
+      type: "string"
+    },
+    name: {
+      type: "string"
+    },
+    type: {
+      type: "string"
+    },
+    userId: {
+      type: "string"
+    },
+    age: {
+      type: "number"
+    }
+  }
 }
