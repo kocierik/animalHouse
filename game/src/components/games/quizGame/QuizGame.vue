f2701eaaf9cd7f2b220e764ec4faa17c385529ba
mattia <m@tt.ia>
2022-11-07 22:13:02 +0100

    feat: wip

--- game/src/components/games/quizGame/QuizGame.vue
+++ game/src/components/games/quizGame/QuizGame.vue
@@ -1,98 +1,64 @@
 <script lang="ts" setup>
-import { Questions } from './utility'
 import { Api, Helpers, GameConstant, ApiRepository, JsonGames } from 'shared'
 import swal from 'sweetalert'
 import { ref, onBeforeMount } from 'vue'
 
-let fetchDone = ref(false)
-let idx = ref(0)
-let selectedAnswer = ref('')
-let correctAnswers = ref(0)
-let wrongAnswers = ref(0)
-let count = ref(5)
-let difficulty = ref('medium')
-let questions = ref(Questions)
-let gameState = ref<any>(null)
-let items = ref<any>([])
+interface ApiResponse {
+  response_code: number,
+  results: ApiQuestion[]
+}
 
-const getQuestion = async () => {
-  // get questions for quiz
-  let response = await Api.get<any>(
-    'https://opentdb.com/api.php?amount=' + count.value + '&category=27&type=multiple&difficulty=' + difficulty.value
-  )
-  if (response.esit) {
-    //adjust every question
-    for (let i = 0; i < count.value; i++) {
-      let q = response.data.results[i]
+interface ApiQuestion {
+    category:          string;
+    type:              string;
+    difficulty:        string;
+    question:          string;
+    correct_answer:    string;
+    incorrect_answers: string[];
+}
 
-      questions.value[i].question = q.question // set questions
-      switch (Math.floor(Math.random() * 4)) {
-        case 0:
-          questions.value[i].answers[0] = q.correct_answer
-          questions.value[i].answers[1] = q.incorrect_answers.shift()
-          questions.value[i].answers[2] = q.incorrect_answers.shift()
-          questions.value[i].answers[3] = q.incorrect_answers.shift()
-          questions.value[i].correctAnswer = 0
-          break
-        case 1:
-          questions.value[i].answers[0] = q.incorrect_answers.shift()
-          questions.value[i].answers[1] = q.correct_answer
-          questions.value[i].answers[2] = q.incorrect_answers.shift()
-          questions.value[i].answers[3] = q.incorrect_answers.shift()
-          questions.value[i].correctAnswer = 1
-          break
-        case 2:
-          questions.value[i].answers[0] = q.incorrect_answers.shift()
-          questions.value[i].answers[1] = q.incorrect_answers.shift()
-          questions.value[i].answers[2] = q.correct_answer
-          questions.value[i].answers[3] = q.incorrect_answers.shift()
-          questions.value[i].correctAnswer = 2
-          break
-        case 3:
-          questions.value[i].answers[0] = q.incorrect_answers.shift()
-          questions.value[i].answers[1] = q.incorrect_answers.shift()
-          questions.value[i].answers[2] = q.incorrect_answers.shift()
-          questions.value[i].answers[3] = q.correct_answer
-          questions.value[i].correctAnswer = 3
-          break
-      }
+interface QuizQuestion {
+  id: number
+  question: string
+  answers: string[]
+  correct: number
+  reveled: boolean
+}
+
+const _COUNT = 5
+const _URL = "https://opentdb.com/api.php?amount=5&category=27&type=multiple&difficulty=medium"
+
+const questions = ref<QuizQuestion[]>()
+const progress = ref<number>(0)
+const correct = ref<number>(0)
+
+const fetchQuestions = async () => {
+    questions.value = []
+    const resp = await Api.get<ApiResponse>(_URL)
+    if (resp.esit && resp.data)  {
+      questions.value = resp.data.results.map(mapQuestion) 
+    } else {
+      // TODO
     }
-    fetchDone.value = true
-    showQuiz()
   }
-}
 
-const swap = (first: any, second: any) => {
-  const tmp = first
-  first = second
-  second = tmp
-}
-
-const answered = (e: any) => {
-  selectedAnswer.value = e.target.value
-  items.value[questions.value[idx.value].correctAnswer].style.backgroundColor = 'lightgreen'
-  if (selectedAnswer.value == questions.value[idx.value].correctAnswer) {
-    correctAnswers.value++
-  } else {
-    wrongAnswers.value++
+const mapQuestion = (q : ApiQuestion, id: number) : QuizQuestion => {
+  const x = Math.random() % 4 
+  const answers = q.incorrect_answers.splice(x, 0, q.correct_answer)
+  return {
+    id: id,
+    question: q.question,
+    answers: answers,
+    correct: x,
+    reveled: false
   }
-}
-
-const nextQuestion = () => {
-  items.value[questions.value[idx.value].correctAnswer].style.backgroundColor = ''
-  idx.value++
-  selectedAnswer.value = ''
-  document.querySelectorAll('input').forEach((el) => (el.checked = false))
 }
 
 const showResults = () => {
-  items.value[questions.value[idx.value].correctAnswer].style.backgroundColor = ''
-  // const msg = `Correct Answers: ${correctAnswers.value}  \n\n  Wrong Answers: ${wrongAnswers.value} `
-  const points = correctAnswers.value
   if (Helpers.isLogged()) {
     swal({
       title: 'Good job!',
-      text: `You have response correctly to ${correctAnswers.value} answers! Do you want save your record?`,
+      text: `You have response correctly to ${correct.value} answers! Do you want save your record?`,
       icon: 'warning',
       buttons: [true],
       dangerMode: false,
@@ -100,7 +66,7 @@ const showResults = () => {
       if (willSave) {
         let totalScore: JsonGames.IGameResult = {
           gameId: GameConstant.QUIZGAME,
-          score: points,
+          score: correct.value,
         }
         const userId = Helpers.getUserId()
         if (!userId) return
@@ -116,91 +82,95 @@ const showResults = () => {
   } else {
     swal({
       title: 'Good job!',
-      text: `You have response correctly to ${correctAnswers.value} answers!`,
+      text: `You have response correctly to ${correct.value} answers!`,
       icon: 'warning',
       dangerMode: false,
     })
   }
-  resetQuiz()
-  getQuestion()
 }
 
-const resetQuiz = () => {
-  idx.value = 0
-  selectedAnswer.value = ''
-  correctAnswers.value = 0
-  wrongAnswers.value = 0
-  questions.value = Questions
-  //fetchAPIData.value = false
-  getQuestion()
-}
-const showQuiz = () => {
-  gameState.value.style.visibility = 'visible'
+const answered = (id: number, guess: number) => {
+  if (!questions.value) return
+
+  questions.value[id].reveled = true
+
+  if (guess === questions.value[id].correct)
+    correct.value ++
 }
 
-onBeforeMount(() => {
-  getQuestion()
-})
+const goToNextQuestion = () => {
+  progress.value++
+  if (progress.value >= _COUNT) {
+    showResults()
+  }
+}
+
+onBeforeMount(async () => {await fetchQuestions()})
 </script>
 
 <template class="animate-in fade-in zoom-in duration-500 antialiased text-gray-700 bg-gray-100">
   <div class="flex w-full h-screen justify-center items-center">
     <div class="w-full max-w-xl p-3 justify-center items-center">
       <div>
-        <h1 class="font-bold text-5xl text-center text-indigo-700">Simple Quiz</h1>
-        <div class="bg-white p-10 rounded-lg shadow-lg w-full mt-1" ref="gameState" :style="{ visibility: 'hidden' }">
-          <div v-if="idx < count">
-            <p class="text-2xl font-bold" v-html="questions[idx]['question']"></p>
+        <h1 class="font-bold text-5xl text-center text-black">Quiz</h1>
+        <div class="bg-white p-10 rounded-lg shadow-lg w-full mt-1" >
+
+          <!-- Questions -->
+          <div v-if="progress < _COUNT && questions && questions[progress]">
+            <p class="text-2xl font-bold"> {{questions[progress].question}}</p>
             <label
-              v-for="[answer, index] in questions[idx].answers"
+              v-for="[answer, index] in questions[progress].answers"
               :key="index"
               :for="index"
               ref="items"
-              class="block cursor-pointer mt-4 border border-gray-300 rounded-lg py-2 px-6 text-lg"
-              :class="{ 'bg-red-200': selectedAnswer == index }"
-            >
+              :class="'block cursor-pointer mt-4 border border-gray-300 rounded-lg py-2 px-6 text-lg ' +
+                questions[progress].reveled 
+                ? (questions[progress].correct === Number(index)? 'bg-green' : 'bg-red')
+                : 'bg-white'">
+
               <input
                 :id="index"
                 type="radio"
                 class="hidden"
                 :value="index"
-                @change="answered($event)"
-                :disabled="selectedAnswer != ''"
+                @change="answered(questions[progress].id, Number(index))"
               />
-              <span v-html="answer"></span>
+
+              <span>{{ answer }}</span>
             </label>
             <div class="mt-6 flow-root">
               <button
-                @click="nextQuestion"
-                v-show="selectedAnswer != '' && idx < count - 1"
+                @click="goToNextQuestion"
+                v-show="progress < _COUNT - 1"
                 class="float-right bg-indigo-600 text-white text-sm font-bold tracking-wide rounded-full px-5 py-2"
               >
                 Next &gt;
               </button>
               <button
                 @click="showResults"
-                v-show="selectedAnswer != '' && idx == count - 1"
+                v-show="progress < _COUNT - 1"
                 class="float-right bg-indigo-600 text-white text-sm font-bold tracking-wide rounded-full px-5 py-2"
               >
                 Finish
               </button>
             </div>
           </div>
+
+          <!-- END -->
           <div v-else>
             <h2 class="text-bold text-3xl">Results</h2>
             <div class="flex justify-start space-x-4 mt-6">
               <p>
                 Correct Answers:
-                <span class="text-2xl text-green-700 font-bold">{{ correctAnswers }}</span>
+                <span class="text-2xl text-green-700 font-bold">{{ correct }}</span>
               </p>
               <p>
                 Wrong Answers:
-                <span class="text-2xl text-red-700 font-bold">{{ wrongAnswers }}</span>
+                <span class="text-2xl text-red-700 font-bold">{{ _COUNT - correct }}</span>
               </p>
             </div>
             <div class="mt-6 flow-root">
               <button
-                @click="resetQuiz"
                 class="float-right bg-indigo-600 text-white text-sm font-bold tracking-wide rounded-full px-5 py-2"
               >
                 Play again
