1286b3d8cb3a00af83e06b57c95f0d6c11de2111
kocierik <kocierik@gmail.com>
2022-11-13 22:23:04 +0100

    chore: insert components

--- front-office/src/views/Service.tsx
+++ front-office/src/views/Service.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import CardService from './common/serviceComponents/CardService'
+import FeatureCard from './common/serviceComponents/FeatureCard'
 
 const Service = () => {
   return (
     <div data-aos="zoom-in" data-aos-duration="500">
-      Service
+      <div className='p-10'>
+        <FeatureCard/>
+      </div>
+      <div>
+        <CardService /> 
+      </div>
     </div>
   )
 }
