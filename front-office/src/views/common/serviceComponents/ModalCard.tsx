efb49d5d4b8a38cb0a1c08e62c65639fd337cb1b
kocierik <kocierik@gmail.com>
2022-11-15 16:35:18 +0100

    chore: fix scroll

--- front-office/src/views/common/serviceComponents/ModalCard.tsx
+++ front-office/src/views/common/serviceComponents/ModalCard.tsx
@@ -9,7 +9,7 @@ const ModalCard = (props :{showModal: boolean, setShowModal: any}) => {
 
   return (
     <>
-                <div className="py-12 flex items-center  transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" ref={openRef!}>
+                <div className="py-12 flex   transition duration-150 ease-in-out z-10 absolute top-20 right-0 bottom-0 left-0" ref={openRef!}>
                 <div role="alert" className="container mx-auto w-11/12 md:w-2/3 max-w-lg">
                     <div className="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
                         <div className="w-full flex justify-start text-gray-600 mb-3">
