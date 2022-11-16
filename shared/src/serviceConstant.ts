547f4ee5164a426161b14a931f4ca35c118e5491
kocierik <kocierik@gmail.com>
2022-11-16 23:41:44 +0100

    chore: add const service

---
+++ shared/src/serviceConstant.ts
@@ -0,0 +1,7 @@
+export const FIND_PARTNER = 'a2f3c0540ac73a2bc4764daa'
+export const HELP_ANIMAL = 'b2f3c0540ac73a2bc4764dab'
+export const DOG_SITTER = 'c2f3c0540ac73a2bc4764dac'
+export const VETERINARY = 'd2f3c0540ac73a2bc4764dad'
+export const WASH_ANIMAL = 'e2f3c0540ac73a2bc4764dae'
+export const HOME_VISIT = 'f2f3c0540ac73a2bc4764daf'
+export const GROOMING = 'g2f3c0540ac73a2bc4764dag'
