421bf98f16e67955f814bf0cabe98e0cab190a34
kocierik <kocierik@gmail.com>
2022-11-16 14:38:45 +0100

    feat: add jsonReservation

---
+++ server/src/json/JsonReservation.ts
@@ -0,0 +1,69 @@
+export interface JsonAddress {
+  country: string
+  city: string
+  street: string
+  zip: number
+}
+
+export interface ILocation {
+  name: string
+  address: JsonAddress
+}
+
+export interface JsonReservation {
+  animalName: string
+  serviceName: string
+  userId: string
+  date: Date
+  information: string
+  location: ILocation
+}
+
+export const SwaggerReservation = {
+  type: 'object',
+  properties: {
+    _id: {
+      type: 'string',
+    },
+    animalName: {
+      type: 'string',
+    },
+    serviceName: {
+      type: 'string',
+    },
+    userId: {
+      type: 'string',
+    },
+    date: {
+      type: 'number',
+    },
+    information: {
+      type: 'string',
+    },
+    location: {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        address: {
+          type: 'object',
+          properties: {
+            country: {
+              type: 'string'
+            },
+            city: {
+              type: 'string'
+            },
+            street: {
+              type: 'string'
+            },
+            zip: {
+              type: 'number'
+            }
+          }
+        }
+      }
+    },
+  },
+}
