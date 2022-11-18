9bc0821c5140bc0c93147859ce60ddc232a86dee
kocierik <kocierik@gmail.com>
2022-11-18 16:43:35 +0100

    chore: remove animation

--- front-office/src/views/common/serviceComponents/ModalCard.tsx
+++ front-office/src/views/common/serviceComponents/ModalCard.tsx
@@ -120,11 +120,11 @@ const ModalCard = (props :{showModal: boolean, setShowModal: any, openService: s
                             }
                         </select>
                         </div>
-                        <div data-aos="zoom-in" className="flex items-center justify-start w-full">
+                        <div  className="flex items-center justify-start w-full">
                             <button onClick={async() => await postReservation()} className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">Submit</button>
                             <button  className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" onClick={() => props.setShowModal(!props.showModal)}>Cancel</button>
                         </div>
-                        <button data-aos="zoom-in" className="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onClick={() => props.setShowModal(!props.showModal)} aria-label="close modal" role="button">
+                        <button  className="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onClick={() => props.setShowModal(!props.showModal)} aria-label="close modal" role="button">
                             <svg  xmlns="http://www.w3.org/2000/svg"  className="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" />
                                 <line x1="18" y1="6" x2="6" y2="18" />
