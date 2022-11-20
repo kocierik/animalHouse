15d5d64fb652ae0042fd069421d7f0bda64e9470
kocierik <kocierik@gmail.com>
2022-11-20 13:31:10 +0100

    chore: add svg

--- front-office/src/views/common/serviceComponents/EditmodalReservationCard.tsx
+++ front-office/src/views/common/serviceComponents/EditmodalReservationCard.tsx
@@ -84,10 +84,18 @@ const EditmodalReservationCard = (props :{showModal: boolean, setShowModal: any,
             <div data-aos="fade-in" className="py-12 flex   transition duration-150 ease-in-out z-10 absolute top-20 right-0 bottom-0 left-0" ref={openRef!}>
                 <div data-aos="zoom-in" role="alert" className="container mx-auto w-11/12 md:w-2/3 max-w-lg">
                     <div data-aos="zoom-in" className="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+                     <div className='flex flex-1 justify-end'>
+                     <svg  onClick={() => props.setShowModal(false)} xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x cursor-pointer" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                      <path stroke="none" d="M0 0h24v24H0z" />
+                                      <line x1="18" y1="6" x2="6" y2="18" />
+                                      <line x1="6" y1="6" x2="18" y2="18" />
+                                  </svg>
+                                  </div>
                         <h1 className="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">{props.openService.charAt(0).toUpperCase() +props.openService.slice(1)}</h1>
                                  <ToastContainer/>
                             <label htmlFor="selectService" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Select a Service</label>
                             <select onChange={(value) => { setSelectedService(value.target.value); getSelectedService(value.target.value); }} className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border">
+                                <option>Select...</option>
                                   {props.animalReservation.map((service, i) => {
                                       return (
                                           <option key={i} value={service._id}>
@@ -127,11 +135,6 @@ const EditmodalReservationCard = (props :{showModal: boolean, setShowModal: any,
                                   <button onClick={async () => await editReservation()} className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">Submit</button>
                                   <button className="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" onClick={() => props.setShowModal(!props.showModal)}>Cancel</button>
                               </div><button className="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600" onClick={() => props.setShowModal(!props.showModal)} aria-label="close modal" role="button">
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                      <path stroke="none" d="M0 0h24v24H0z" />
-                                      <line x1="18" y1="6" x2="6" y2="18" />
-                                      <line x1="6" y1="6" x2="18" y2="18" />
-                                  </svg>
                               </button></div>
                         )}
                     </div>
