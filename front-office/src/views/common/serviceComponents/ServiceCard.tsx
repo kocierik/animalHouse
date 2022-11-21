52bac792d5239208f503dfa6fc170b7fac52d6b0
kocierik <kocierik@gmail.com>
2022-11-21 16:12:11 +0100

    feat: add icon

--- front-office/src/views/common/serviceComponents/ServiceCard.tsx
+++ front-office/src/views/common/serviceComponents/ServiceCard.tsx
@@ -1,101 +1,27 @@
 import React, { useEffect, useRef, useState } from 'react'
 import ModalReservationCard from './ModalReservationCard'
 import { ApiRepository, JsonService} from 'shared';
+import { FaBriefcase } from 'react-icons/fa';
+import { BanIcon, CakeIcon, FilmIcon, FireIcon, FlagIcon, GiftIcon, HomeIcon, MapIcon, SunIcon } from '@heroicons/react/solid';
 
-const serviceProps = [{
-    title: "Find Partner",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-red-500 text-white shadow-lg shadow-red-200",
-    icon:<svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                    </svg>
+const serviceIcon = [{
+    icon: <CakeIcon height={20}/>
 },{
-    title: "Help animals",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-green-500 text-white shadow-lg shadow-green-200",
-    icon:                    <svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                        <polyline points="14 2 14 8 20 8"></polyline>
-                        <line x1="16" y1="13" x2="8" y2="13"></line>
-                        <line x1="16" y1="17" x2="8" y2="17"></line>
-                        <polyline points="10 9 9 9 8 9"></polyline>
-                    </svg>,
-
+    icon: <MapIcon height={20}/>
 },{
-    title: "psychologist",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-indigo-500 text-white shadow-lg shadow-indigo-200",
-    icon:                    <svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
-                    </svg>,
+    icon: <BanIcon height={20}/>
 },{
-    title: "Dog Sitter",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200",
-    icon:                    <svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
-                    </svg>,
+    icon: <SunIcon height={20}/>
 },{
-    title: "veterinary",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-blue-500 text-white shadow-lg shadow-blue-200",
-    icon:                    <svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                    </svg>,
+    icon: <FireIcon height={20}/>
 },{
-    title: "Pension",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-orange-500 text-white shadow-lg shadow-orange-200",
-    icon:                    <svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                    </svg>,
+    icon: <FilmIcon height={20}/>
 },{
-    title: "Wash Animal",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200",
-    icon:                    <svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                    </svg>,
+    icon: <GiftIcon height={20}/>
 },{
-    title: "Home visit",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-pink-500 text-white shadow-lg shadow-pink-200",
-    icon:                    <svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                    </svg>,
-},
-{
-    title: "Grooming",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    color: "p-5 rounded-full bg-emerald-500 text-white shadow-lg shadow-emerald-200",
-    icon:                    <svg
-                        xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" strokeWidth="1.5">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                    </svg>,
+    icon: <FlagIcon height={20}/>
+},{
+    icon: <HomeIcon height={20}/>
 },
 ]
 
@@ -136,7 +62,7 @@ const ServiceCard = () => {
             data-aos="zoom-in"
                 className="p-10   rounded flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-pointer">
                 <span className={service.color}>
-                    {/* {service.icon} */}
+                    {serviceIcon[i].icon}
                 </span>
                 <p className="text-xl font-medium text-slate-700 mt-3">{service.title}</p>
                 <p className="mt-2 text-sm text-slate-500">{service.description}</p>
--- server/src/initial-migrations.ts
+++ server/src/initial-migrations.ts
@@ -161,37 +161,45 @@ export const initReservationCodes = async () => {
   await ReservationCode.insertMany([
     {     title: "Find Partner",
           description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-          color: "p-5 rounded-full bg-red-500 text-white shadow-lg shadow-red-200"},
+          color: "p-5 rounded-full bg-red-500 text-white shadow-lg shadow-red-200",
+    },
     { title: "Help animals",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      color: "p-5 rounded-full bg-green-500 text-white shadow-lg shadow-green-200"},
+      color: "p-5 rounded-full bg-green-500 text-white shadow-lg shadow-green-200",
+    },
     { title: "psychologist",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      color: "p-5 rounded-full bg-indigo-500 text-white shadow-lg shadow-indigo-200"},
-
+      color: "p-5 rounded-full bg-indigo-500 text-white shadow-lg shadow-indigo-200",
+    },
     { title: "Dog Sitter",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled  it to make a type specimen book.",
-      color: "p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200"},
+      color: "p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200",
+    },
     {     
       title: "veterinary",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      color: "p-5 rounded-full bg-blue-500 text-white shadow-lg shadow-blue-200"},
+      color: "p-5 rounded-full bg-blue-500 text-white shadow-lg shadow-blue-200",
+    },
     {
       title: "Pension",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      color: "p-5 rounded-full bg-orange-500 text-white shadow-lg shadow-orange-200"},
+      color: "p-5 rounded-full bg-orange-500 text-white shadow-lg shadow-orange-200",
+    },
     {
       title: "Wash Animal",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      color: "p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200"},
+      color: "p-5 rounded-full bg-yellow-500 text-white shadow-lg shadow-yellow-200",
+    },
     {     
       title: "Home visit",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      color: "p-5 rounded-full bg-pink-500 text-white shadow-lg shadow-pink-200"},
+      color: "p-5 rounded-full bg-pink-500 text-white shadow-lg shadow-pink-200",
+    },
     {     
       title: "Grooming",
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      color: "p-5 rounded-full bg-emerald-500 text-white shadow-lg shadow-emerald-200"},
+      color: "p-5 rounded-full bg-emerald-500 text-white shadow-lg shadow-emerald-200",
+    },
   ])
 }
 
