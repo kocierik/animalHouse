a37bad5fb24509596228410328c24640b493ad93
Gabriele Crestanello <gabricre01@gmail.com>
2023-01-19 14:42:59 +0100

    feat: merge

--- server/src/entities/Address.ts
+++ server/src/entities/Address.ts
@@ -4,14 +4,14 @@ export interface IAddress {
   country: string
   city: string
   street: string
-  zip: number
+  zip: string
 }
 
 export const addressSchema = new Schema<IAddress>({
   country: { type: String, required: true },
   city: { type: String, required: true },
   street: { type: String, required: true },
-  zip: { type: Number, required: true },
+  zip: { type: String, required: true },
 })
 
 export const Address = model<IAddress>('Address', addressSchema)
