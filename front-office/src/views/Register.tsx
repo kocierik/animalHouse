229d5a354c12a898bc4c2b0bee96ffe1bb828198
kocierik <kocierik@gmail.com>
2023-01-19 12:06:03 +0100

    fix: login

--- front-office/src/views/Register.tsx
+++ front-office/src/views/Register.tsx
@@ -54,9 +54,7 @@ const Register = () => {
           <div
             className="hidden bg-cover lg:block lg:w-2/4"
             style={{
-              //border-radius: 1rem;
-              backgroundImage:
-                'url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=htmlFormat&fit=crop&w=1470&q=80)'
+              backgroundImage: "url('/login.jpg')"
             }}
           >
             <div
@@ -64,11 +62,11 @@ const Register = () => {
               className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40"
             >
               <div>
-                <h2 className="text-4xl font-bold text-white">Brand</h2>
+                <h2 className="text-4xl font-bold text-white">Register</h2>
 
                 <p className="max-w-xl mt-3 text-gray-300">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem ipsa, nulla laboriosam dolores,
-                  repellendus perferendis libero suscipit nam temporibus molestiae
+                  Welcome to our animal website! To access all of our features and content, please log in to your account.
+                   If you do not have an account yet, please sign up for one. 
                 </p>
               </div>
             </div>
@@ -76,10 +74,6 @@ const Register = () => {
 
           <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
             <div className="flex-1">
-              <div className="text-center">
-                <h2 className="text-4xl font-bold text-center text-gray-700">Nice to meet you!</h2>
-                <p className="mt-3 text-gray-500">Tell me more about you</p>
-              </div>
               <div className="mt-8">
                 {isError ? <ErrorBox text={error} /> : <div />}
                 {/* Name */}
