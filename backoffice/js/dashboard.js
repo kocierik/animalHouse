b744abdb3eee0360eca6d0deb2544bebec2034ff
Gabriele Crestanello <gabricre01@gmail.com>
2023-01-22 14:53:36 +0100

    feat: number of locations on dashboard

--- backoffice/js/dashboard.js
+++ backoffice/js/dashboard.js
@@ -1,13 +1,25 @@
 
 getCustomersNumber()
+getLocationsNumber()
 
 function getCustomersNumber(){
     var url = "/api/v2/users/";
     fetch(url, {
         headers: {
-            'authorization': localStorage.token
+            'authorization': localStorage.bo_token
         }
     }).then((response) => response.json()).then((data) => {
         document.getElementById("customers_place").innerHTML = data.length
+    });
+}
+
+function getLocationsNumber(){
+    var url = "/api/v2/locations/";
+    fetch(url, {
+        headers: {
+            'authorization': localStorage.bo_token
+        }
+    }).then((response) => response.json()).then((data) => {
+        document.getElementById("locations_place").innerHTML = data.length
     });
 }
