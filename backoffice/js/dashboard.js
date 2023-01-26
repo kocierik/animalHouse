6db3f11bc98dc991fc48248cacfc49a8b5bbc0a0
Gabriele Crestanello <gabricre01@gmail.com>
2023-01-26 15:33:16 +0100

    feat: showing revenue on dashboard and order date converted

--- backoffice/js/dashboard.js
+++ backoffice/js/dashboard.js
@@ -1,8 +1,14 @@
+var orders = []
+main()
 
-getCustomersNumber()
-getLocationsNumber()
+async function main(){
+    getSales()
+    showCustomersNumber()
+    showLocationsNumber()
+}
 
-function getCustomersNumber(){
+
+function showCustomersNumber(){
     var url = "/api/v2/users/";
     fetch(url, {
         headers: {
@@ -13,7 +19,7 @@ function getCustomersNumber(){
     });
 }
 
-function getLocationsNumber(){
+function showLocationsNumber(){
     var url = "/api/v2/locations/";
     fetch(url, {
         headers: {
@@ -22,4 +28,30 @@ function getLocationsNumber(){
     }).then((response) => response.json()).then((data) => {
         document.getElementById("locations_place").innerHTML = data.length
     });
+}
+
+async function getSales(){
+    var url = "/api/v2/orders/";
+    await fetch(url, {
+        headers: {
+            'authorization': localStorage.bo_token
+        }
+    }).then((response) => response.json()).then(async (data) => {
+        document.getElementById("sales_place").innerHTML = data.length
+        orders = await data
+        showRevenue()
+    });
+}
+
+async function showRevenue(){
+    var total = 0
+    console.log(orders)
+    orders.forEach(el => {
+        el.executionDate = new Date(el.executionDate)   //converting executionDate from "ddd MM DD YYYY" to Date
+        el.cartItems.forEach(i => {
+            total+=i.price
+        });
+    });
+    console.log(orders)
+    document.getElementById("revenue_place").innerHTML = `${total}$`
 }
